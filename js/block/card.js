9ec63eee092a4ff7ae979f3be10b2f062a0ccf84
Max Igor <maxigor.ferreira@gmail.com>
2016-03-08 22:57:15 -0300

    remove button

--- js/block/card.js
+++ js/block/card.js
@@ -1,12 +1,12 @@
 var $cards = document.querySelectorAll('.card');
-var $cardColors = document.querySelectorAll('.card-colors');
+var $cardColors = document.querySelectorAll('.card-options');
 
 for (var i = 0; i < $cards.length; i++) {
    $cards[i].addEventListener('click', function(event){
       var $this = event.target;
+      var $card = this;
 
-      if ($this.classList.contains('card-colors')) {
-         var $card = this;
+      if ($this.dataset.color) {
 
          $card.dataset.color = $this.dataset.color;
 
@@ -16,6 +16,8 @@ for (var i = 0; i < $cards.length; i++) {
 
          $this.classList.add('isActive');
       };
-
+      if($this.classList.contains('card_delete')){
+         $card.remove();
+      }
    });
 };
